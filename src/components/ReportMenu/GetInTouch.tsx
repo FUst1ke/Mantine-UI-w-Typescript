018523dd1a1313d6509af0508fb6c1b0c397cb8f
FUst1ke <fustigithub@gmail.com>
2023-11-12 22:54:13 +0100

    input checks, deleted unused imports and logics

--- src/components/ReportMenu/GetInTouch.tsx
+++ src/components/ReportMenu/GetInTouch.tsx
@@ -1,9 +1,25 @@
-import { Paper, Text, TextInput, Textarea, Button, Group, SimpleGrid } from '@mantine/core'; // iconok hívása
+import { Paper, Text, TextInput, Textarea, Button, Group, SimpleGrid, NumberInput } from '@mantine/core'; // iconok hívása
 import { ContactIconsList } from './ContactIcons'; // ContactIconList.tsx
-import bg from './bg.svg'; // unused, nincs meg az svg xd
 import classes from './GetInTouch.module.css'; // css
+import { useForm } from '@mantine/form';
 
 export function GetInTouch() {
+  const form = useForm({
+    initialValues: {
+      name: '',
+      phoneNumber: undefined,
+      subject: '',
+      description: ''
+    },
+
+    validate: (values) => ({
+      name: (0 == values.name.length ? 'Érvénytelen adat': null),
+      phoneNumber: (values.phoneNumber === undefined ? 'Age is required' : values.phoneNumber == 0 ? 'Érvénytelen adat' : null),
+      subject: (0 == values.subject.length ? 'Érvénytelen adat': null),
+      description: (0 == values.description.length ? 'Érvénytelen adat': null)
+    })
+  });
+
   return (
     <Paper radius="lg">
       <div className={classes.wrapper}>
@@ -15,25 +31,25 @@ export function GetInTouch() {
           <ContactIconsList />
         </div>
 
-        <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
+        <form className={classes.form} onSubmit={form.onSubmit((values) => console.log("VALUES", values))}>
           <Text fz="lg" fw={700} className={classes.title}>
             Get in touch
           </Text>
 
           <div className={classes.fields}>
             <SimpleGrid cols={{ base: 1, sm: 2 }}>
-              <TextInput label="Your name" placeholder="Your name" required/>
-              <TextInput label="Your email" placeholder="hello@mantine.dev" required />
+              <TextInput {...form.getInputProps('name')} label="Your name" placeholder="Your name"/>
+              <NumberInput {...form.getInputProps('phoneNumber')} label="Your phone number" hideControls placeholder="Your phone number" />
             </SimpleGrid>
 
-            <TextInput mt="md" label="Subject" placeholder="Subject" required />
+            <TextInput {...form.getInputProps('subject')} mt="md" label="Subject" placeholder="Subject" />
 
             <Textarea
+              {...form.getInputProps('description')}
               mt="md"
               label="Your message"
               placeholder="Please include all relevant information"
               minRows={3}
-              required
             />
 
             <Group justify="flex-end" mt="md">
